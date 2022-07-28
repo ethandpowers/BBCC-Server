31654dbc292de9370c2bf61d0813d947f3d965c0
Ethan Powers <ethandpowers@gmail.com>
2022-07-28 16:12:02 -0400

    switching pc

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const app = require('express')();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+import { Server } from "socket.io";
+const io = new Server(3000);
+
+// create a new room when a "host" connects
+io.on("connection", (socket) => {
+
+    // when a "host" connects, create a new room
+    socket.on("host", () => {
+        //generate room id
+        const roomId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+        
+        console.log("roomId: " + roomId);
+        socket.join(roomId);
+    });
+});
